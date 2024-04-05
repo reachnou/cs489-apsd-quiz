55964eb5519641dd1e8e9921077e756ad5250f0c
noureach <rnou@miu.edu>
2024-04-05 13:46:21 -0500

    set up project

---
+++ apsd-quiz1/src/main/java/edu/miu/cs489/apsdquiz1/ApsdQuiz1Application.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.apsdquiz1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsdQuiz1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApsdQuiz1Application.class, args);
+    }
+
+}
---
+++ apsd-quiz1/src/test/java/edu/miu/cs489/apsdquiz1/ApsdQuiz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.apsdquiz1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsdQuiz1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
